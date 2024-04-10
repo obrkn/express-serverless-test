554dbc50920dc5d9a3e7b091c7d94d8972e3ae77
obrkn <k33mdukb9n@yahoo.co.jp>
2024-04-10 14:17:12 -0700

    imken

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const serverless = require('serverless-http'); // <- 追加
-const express = require('express');
+const serverless = require("serverless-http"); // <- 追加
+const express = require("express");
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+app.get("/", (req, res) => {
+  res.send("Hello Im ken");
 });
 
-const port = '8080';
+const port = "8080";
 app.listen(port, () => {
   console.log(`app start listening on port ${port}`);
 });
 
-module.exports.handler = serverless(app);  // <- 追加
-
+module.exports.handler = serverless(app); // <- 追加
